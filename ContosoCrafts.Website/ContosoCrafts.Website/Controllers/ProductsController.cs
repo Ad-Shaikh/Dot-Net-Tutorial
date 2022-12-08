f9442b641a07699dece5ff5b6bdb36e1507ce4bb
Adnan Shaikh <adnan.sk.0909@gmail.com>
2022-12-08 15:50:11 +0530

    Added Blazor Component

---
+++ ContosoCrafts.Website/ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) =>
+            ProductService = productService;
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => ProductService.GetProducts();
+
+        //[HttpPatch]
+        //public ActionResult Patch([FromBody] RatingRequest request)
+        //{
+        //    if (request?.ProductId == null)
+        //        return BadRequest();
+
+        //    ProductService.AddRating(request.ProductId, request.Rating);
+
+        //    return Ok();
+        //}
+
+        [Route("Rate")]
+        [HttpGet]
+
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok(); 
+        }
+
+        public class RatingRequest
+        {
+            public string? ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
